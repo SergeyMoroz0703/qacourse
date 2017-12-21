cfd681b0faf18427deb75a4160f57cf6152425d6
sergeymoroz <sergeymoroz0307@gmail.com>
2017-12-21 10:54:38 +0200

    refactor 8-12v1

--- pytasks.py
+++ pytasks.py
@@ -110,165 +110,169 @@ def caesar_cipher(datastring, key):
 # Using random.randint(A, B) generate random int from the range.While user input isn't equal that number,
 # print "Try again!". If user guess the number, congratulate him and exit. (use raw_input())
 
-# import random
-#
-#
-# def game(start, finish):
-#     pc_cohice = random.randint(start, finish)
-#     #print(pc_cohice)
-#     user_choice = input('Please, enter the digit: ')
-#     try:
-#         if int(pc_cohice) == int(user_choice):
-#             print('You win!')
-#         else:
-#             print('Fucking shit, you lose. Come back later!')
-#     except ValueError:
-#         print('Please enter the valid integer number!')
-#         game(start,finish)
-#
-# game (3,7)
+def game(start, finish):
+    pc_cohice = random.randint(start, finish)
+    user_choice = input('Please, enter the digit: ')
+    try:
+        if int(pc_cohice) == int(user_choice):
+            return ('You win!')
+        else:
+            return ('Fucking shit, you lose. Come back later!')
+    except ValueError:
+        print ('Please enter the valid integer number!')
+        game(start, finish)
+
+
 
 #9.Define a function, which takes a string with N opening brackets ("[") and N closing brackets ("]"), in some arbitrary order.
 #Determine whether the generated string is balanced; that is, whether it consists entirely of pairs
 #  of opening/closing brackets (in that order), none of which mis-nest.
 
-# def determ(string):
-#     a = '['
-#     b = ']'
-#     num_a = 0
-#     num_b = 0
-#     for i in string:
-#         if i == a:
-#             num_a = num_a+1
-#         else:
-#             num_b = num_b+1
-#
-#     counter = 0
-#     if string[0] == a and string[len(string) - 1] == b and num_a == num_b:
-#         for i in string:
-#             if i == a:
-#                 counter = counter + 1
-#             else:
-#                 counter = counter -1
-#                 if counter < 0:
-#                     print('NO OK')
-#                     break
-#                 else:
-#                     pass
-#     print(counter)
-#     if counter >= 0:
-#         print('OK')
-#
-# determ('[][][[]][[[]]]')
+def determ(string):
+    a = '['
+    b = ']'
+    num_a = 0
+    num_b = 0
+    for i in string:
+        if i == a:
+            num_a = num_a+1
+        else:
+            num_b = num_b+1
+
+    counter = 0
+    if string[0] == a and string[len(string) - 1] == b and num_a == num_b:
+        for i in string:
+            if i == a:
+                counter = counter + 1
+            else:
+                counter = counter -1
+                if counter < 0:
+                    return ('NO OK')
+                else:
+                    pass
+    else:
+        return ('NO OK')
+    if counter >= 0:
+        return ('OK')
+
+
 
 # 10. Write a function char_freq() that takes a string and builds a frequency listing of the characters contained in it.
 # Represent the frequency listing as a Python dictionary. Try it with something like char_freq("abbabcbdbabdbdbabababcbcbab").
 
+def char_freq(string):
+    count_dict = {}
+    for i in string:
+        count_dict[i] = string.count(i)
+    return (count_dict)
 
-# def char_freq(string):
-#     count_dict = {}
-#     for i in string:
-#         count_dict[i] = string.count(i)
-#     print(count_dict)
-#
-# char_freq('abcdddcbaaa4')
+
 
 #11. Write a function dec_to_bin() that takes decimal integer and outputs its binary representation.
 
+def dec_to_bin_rec(digit):
 
-# def dec_to_bin_rec(digit):
-#     if digit >= 1:
-#         dec_to_bin_rec(digit // 2)
-#         number = int(digit % 2)
-#         print(number, end='')
-#     elif digit == 1:
-#         print('1')
-#
-#
-#
-# dec_to_bin_rec(100)
-#
-# def dec_to_bin_v2(digit):
-#     time_list = []
-#     if digit == 1:
-#         print('1')
-#     else:
-#         while digit >= 1:
-#             number = digit % 2
-#             #print(number)
-#             time_list.append(str(number))
-#             digit = digit // 2
-#     print(''.join(time_list[::-1]))
-#
-#
-#
-#
-#
-# dec_to_bin_v2(100)
+    if digit < 0:
+        return ('Argument should be positive')
+    elif digit == 0:
+        return ''
+    elif digit >=1:
+        return dec_to_bin_rec(digit // 2) + str(digit % 2)
+
+
+def dec_to_bin_v2(digit):
+    time_list = []
+    if digit == 1:
+        return ('1')
+    else:
+        while digit >= 1:
+            number = digit % 2
+            time_list.append(str(number))
+            digit = digit // 2
+    return (''.join(time_list[::-1]))
+
+
+
+
+
+dec_to_bin_v2(100)
 
 # 12. Write a ship battle game, which is similar to ex.8, except it takes 1 integer as an order of matrix, randomly
 # generates index (x, y) and checks user input (2 integers).
 # (tip: use var1, var2 = raw_input("Enter two numbers here: ").split())
 # *Visualize the game.
 
-# import random
-# #
-# #
-# def ship(digit):
-#
-#     new_matrix = [['*' for y in range(digit)] for z in range(digit)]
-#     matrix = [['*' for y in range(digit)] for z in range(digit)]
-#     print('Lets START. This is your field!')
-#     for i in matrix:
-#         print(i)
-#
-#     x = random.randrange(1,digit+1)
-#     y = random.randrange(1,digit+1)
-#     print(x,y)
-#     pc_matrix = matrix
-#     pc_matrix[x-1][y-1] = 'X'
-#     print('Compute made his choice...')
-#
-#
-#     x_u, y_u = input('Please enter two index kill pc machine?! ').split(' ')
-#     i = True
-#     while i:#int(x) != int(x_u) and int(y) != int(y_u):
-#         new_matrix[int(x_u) - 1][int(y_u) - 1] = '-'
-#         for i in new_matrix:
-#             print(i)
-#         x_u, y_u = input('Please try again: ').split(' ')
-#
-#         if int(x) == int(x_u) and int(y) == int(y_u):
-#             pc_matrix[x - 1][y - 1] = 'bah!'
-#             print('You win!')
-#             for i in pc_matrix:
-#                 print(i)
-#             i = False
-#
-#
-#
-#
-# ship(3)
+import random
 
-#Task_1
-#print(greet('Mentor'))
+def ship(digit):
 
-#Task_2
-# print(sum([1,0,2]))
-# print(multiply([1,2,1]))
+    new_matrix = [['*' for y in range(digit)] for z in range(digit)]
+    matrix = [['*' for y in range(digit)] for z in range(digit)]
+    print('Lets START. This is your field!')
+    for i in matrix:
+        print(i)
+
+    x = random.randrange(1,digit+1)
+    y = random.randrange(1,digit+1)
+    #print(x,y)                      #Uncomment to see computer random choice
+    pc_matrix = matrix
+    pc_matrix[x-1][y-1] = 'X'
+    print('Compute made his choice...')
+
+
+    x_u, y_u = input('Please enter two index kill pc machine?! ').split(' ')
+    i = True
+    while i:
+        new_matrix[int(x_u) - 1][int(y_u) - 1] = '-'
+        for i in new_matrix:
+            print(i)
+        x_u, y_u = input('Please try again: ').split(' ')
+
+        if int(x) == int(x_u) and int(y) == int(y_u):
+            pc_matrix[x - 1][y - 1] = 'bah!'
+            print('You win!')
+            for i in pc_matrix:
+                print(i)
+            i = False
+
+
+
+#'Task_1'
+print(greet('Mentor'))
+
+#'Task_2'
+print(sum([1,0,2]))
+print(multiply([1,2,1]))
 
 #Task_3
-#print(reverse('Mentor'))
+print(reverse('Mentor'))
 
 #Task_4
-#print(is_palindrome('radar'))
+print(is_palindrome('radar'))
 
 #Task_5
+print(histogram([1,2,3]))
 
-# print(histogram([1,2,3]))
-
-#Task_6
-
+# #Task_6
 print(caesar_cipher('xyz', 25))
 
+# #Task_7
+
+
+# #Task_8
+print(game (3,5))
+
+#Task_9
+print(determ('[[[[]][]]]]]]'))
+
+#Task_10
+print(char_freq('asssassddddd   asd'))
+
+# #Task_11
+print(dec_to_bin_rec(2))
+
+print(dec_to_bin_v2(2))
+
+# #Task_12
+ship(3)
 
